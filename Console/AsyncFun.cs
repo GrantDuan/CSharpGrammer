22a38bbc3a0363145bfd951e4d95c7f2137e94d5
GrantDuan <xiugang.duan@igt.com>
2020-01-21 11:24:22 +0800

    await Task.Factory.StartNew syncMethod and asyncMethod

---
+++ Console/AsyncFun.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrammerConsole
+{
+  class AsyncFun
+  {
+    public async void Run()
+    {
+      Console.WriteLine("begin");
+
+      //what's the difference of await startnew sync method and await startnew async method?
+      //await Task.Factory.StartNew(LongTimeTask);
+      await Task.Factory.StartNew(LongTimeTaskAsync);
+      Console.WriteLine("end");
+    }
+
+    public void LongTimeTask()
+    {
+      Console.WriteLine("async task begin");
+      Task.Delay(1000);
+      Console.WriteLine("async task end");
+    }
+
+
+    public async void LongTimeTaskAsync()
+    {
+      Console.WriteLine("async task begin");
+      await Task.Delay(1000);
+      Console.WriteLine("async task end");
+    }
+  }
+}
---
+++ Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GrammerConsole
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var af = new AsyncFun();
+      af.Run();
+
+      Console.Read();
+    }
+  }
+}
